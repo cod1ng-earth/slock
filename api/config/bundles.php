43ee0f12c22d75cf458b11280cc1473575c1a7b3
Andreas Möller <am@localheinz.com>
2019-05-18 21:22:47 +0200

    Fix: Enable HautelookAliceBundle for all environments

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,5 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
-    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['all' => true],
 ];
